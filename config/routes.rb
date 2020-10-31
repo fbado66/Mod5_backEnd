031a5668d809b6990bb23e200577bc19b0d06647
fbado66 <fbado66@gmail.com>
2020-10-31 19:24:33 -0400

    cart creates

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
   resources :options
   resources :restaurants
   resources :carts
-  resources :users
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-
+ get '/users', to: 'users#index'
   post "/users/login", to: "users#login"
+  post '/users', to: 'users#create'
   get "/users/keep_logged_in", to: "users#keep_logged_in"
 
 end
