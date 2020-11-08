2db850316cb86b582569ca3a66e4219782ca793d
fbado66 <fbado66@gmail.com>
2020-11-08 13:44:34 -0500

    updating user information

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Rails.application.routes.draw do
   resources :options
   resources :restaurants
   resources :carts
-  # resources :users
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
  get '/users', to: 'users#index'
   post "/users/login", to: "users#login"
   post '/users', to: 'users#create'
+  # get '/users/:id/edit', to: 'users#edit'
   get "/users/keep_logged_in", to: "users#keep_logged_in"
   post '/charges', to: 'charges#create'
   get '/charges', to: 'charges#index'
